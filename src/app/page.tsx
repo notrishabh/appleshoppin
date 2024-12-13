994c033fc8fed5af8f185be48e94c67d1840dd52
Rishabh <dplayarrr@gmail.com>
2024-12-14 03:18:15 +0530

    ADD: Get started animation for watch and text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,23 @@
+"use client";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
+  const [startFlow, setStartFlow] = useState(false);
+  const getStarted = () => {
+    setStartFlow(true);
+  };
   return (
     <section>
       <div className="absolute top-[29px] left-8">
         <Image src="/apple-watch-logo.png" alt="WATCH" width={90} height={0} />
       </div>
       <section className="h-[70vh] flex justify-center items-center flex-col text-left">
-        <div className="absolute top-[17%] z-[2]">
+        <div
+          className={`absolute top-[17%] z-[2] opacity-100 transition-opacity duration-500 ease-in-out delay-300 ${
+            startFlow && "opacity-0"
+          }`}
+        >
           <h1 className="flex flex-col">
             <span className="text-subtitle tracking-tighter leading-snug pb-[10px]">
               Apple Watch Studio
@@ -22,11 +32,16 @@ export default function Home() {
               Create your own style.
             </span>
           </h1>
-          <button className="bg-primary mt-11 py-2 px-[22px] min-w-[70px] text-[17px] tracking-tight rounded-full text-white hover:bg-primaryhover">
+          <button
+            onClick={getStarted}
+            className="bg-primary mt-11 py-2 px-[22px] min-w-[70px] text-[17px] tracking-tight rounded-full text-white hover:bg-primaryhover"
+          >
             Get started
           </button>
         </div>
-        <div className="transform translate-y-[32rem] scale-[2] mt-48">
+        <div
+          className={`transform translate-y-[32rem] scale-[2] mt-48 ${startFlow && "transform-none transition-transform duration-[1200ms] ease-in-out delay-[400ms]"}`}
+        >
           <Image
             src="/main-watch-face.png"
             alt="WATCH"
